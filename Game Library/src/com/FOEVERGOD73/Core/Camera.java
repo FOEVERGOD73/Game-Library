33cf75db6873cad28afaf52832eb230ce779cdef
ethanzhu <ethanzhu@gmail.com>
2015-05-28 21:45:03 -0700

    Made Camera a Singleton
    Implanted Camera into base classes
    
    Documented Camera

--- Game Library/src/com/FOEVERGOD73/Core/Camera.java
+++ Game Library/src/com/FOEVERGOD73/Core/Camera.java
@@ -2,35 +2,36 @@ package com.FOEVERGOD73.Core;
 
 import com.FOEVERGOD73.Objects.GameObject;
 
+/**
+ * Set the target of the Camera instance with the setCameraTarget method.
+ * Also use the xOffset and yOffset to specify the offset of the target from the center of the screen
+ */
 public class Camera {
+	public static final Camera instance = new Camera();
 
 	private double x = 0, y = 0;
 	private double xoffset, yoffset;
 	private boolean xlock, ylock;
-	public Camera(float xoffset, float yoffset){
-	this.xoffset = xoffset;
-	this.yoffset = yoffset;
+	private GameObject cameraTarget = null;
+
+	private Camera() {
 	}
-	
-	public void tick(GameObject cameraTarget){
-		if(!xlock)
-			x = (-cameraTarget.getX() + CoreEngine.WIDTH/2) + xoffset;
-		if(!ylock)
-			y = (-cameraTarget.getY() + CoreEngine.HEIGHT/2)+ yoffset;
+
+	public void tick() {
+		if (cameraTarget != null) {
+			if (!xlock)
+				x = (-cameraTarget.getX() + CoreEngine.WIDTH / 2) + xoffset;
+			if (!ylock)
+				y = (-cameraTarget.getY() + CoreEngine.HEIGHT / 2) + yoffset;
+		}
 	}
-	
-	
- public double getX() {
+
+	public double getX() {
 		return x;
 	}
-	public void setX(double x) {
-		this.x = x;
-	}
+
 	public double getY() {
 		return y;
-	}
-	public void setY(double y) {
-		this.y = y;
 	}
 
 	public boolean isYlock() {
@@ -47,5 +48,21 @@ public class Camera {
 
 	public void setXlock(boolean xlock) {
 		this.xlock = xlock;
+	}
+
+	public double getXoffset() {
+		return xoffset;
+	}
+
+	public void setXoffset(double xoffset) {
+		this.xoffset = xoffset;
+	}
+
+	public double getYoffset() {
+		return yoffset;
+	}
+
+	public void setYoffset(double yoffset) {
+		this.yoffset = yoffset;
 	}
 }
